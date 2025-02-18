c5cead4047841b71b0221b4e0597ec6734729459
Andre Klapper <a9016009@gmx.de>
2025-02-18 19:13:18 +0100

    Add missing pht() to some options
    Change-Id: I15a1fb0ee8c8ae02df8b8b1b6d6ee067f2f3f899

--- src/config/AntiVandalismConfigOptions.php
+++ src/config/AntiVandalismConfigOptions.php
@@ -79,48 +79,48 @@ final class PhabricatorAntiVandalismConfigOptions
         array(
           'description' => 1.0, 'title' => 0.5
         ))
-        ->setSummary(
-          'Default scores applied to edits that change a text field.')
-        ->setDescription(
+        ->setSummary(pht(
+          'Default scores applied to edits that change a text field.'))
+        ->setDescription(pht(
           'These are the default scores applied to edits on text fields such' .
           'as task title or description. The base score is multiplied by a ' .
           'factor which is determined by how much the text was changed. ' .
           'What this means is that edits which only add text are scored ' .
           'lower than edits which remove or alter existing text. The base ' .
-          'score for each field should represent its relative importance.'
+          'score for each field should represent its relative importance.')
         ),
 
       $this->newOption(
         'antivandalism.age-factor-multiplier',
         'int',
         1)
-        ->setSummary(
-          'The multiplier applied to the age component of the score.')
-        ->setDescription('Larger values inflate the overall score. '.
-          'This should be a value between 2 and 10.'),
+        ->setSummary(pht(
+          'The multiplier applied to the age component of the score.'))
+        ->setDescription(pht('Larger values inflate the overall score. '.
+          'This should be a value between 2 and 10.')),
 
       $this->newOption(
         'antivandalism.age-factor-decay',
         'wild',
         '1.2')
-        ->setSummary(
-          'The rate of decay applied to the age component of the score.')
-        ->setDescription(
+        ->setSummary(pht(
+          'The rate of decay applied to the age component of the score.'))
+        ->setDescription(pht(
           'Larger values result in a faster decay which means that older ' .
           'edits score lower.  Each edit is scored, then the score is multiplied '.
           'by the age multiplier. The multiplier is calculated as follows: '.
           'age_factor = multiplier * (age / age^decay). '.
-          'Age is how long ago the edit occurred, in seconds.'),
+          'Age is how long ago the edit occurred, in seconds.')),
 
       $this->newOption('antivandalism.short-text-penalty', 'int', 5)
-        ->setSummary(
+        ->setSummary(pht(
           'This constant is added to the score when an edit results in very '.
-          'short title or description.'),
+          'short title or description.')),
 
       $this->newOption('antivandalism.short-text-length', 'int', 10)
-        ->setSummary(
+        ->setSummary(pht(
           'Minimum length below which a penalty is applied. '.
-          'See also: antivandalism.short-text-penalty'),
+          'See also: antivandalism.short-text-penalty')),
       );
   }
 
